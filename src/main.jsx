6a205a607266eaaf979c1c3be2980886c3d7ac6d
abhiupadhyay-Dev <abhiupadhyayc51@gmail.com>
2025-01-28 12:57:53 +0530

    add AppKit integration and dependencies; implement AppKitProvider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AppKitProvider } from './appkit-setup.jsx'
+
+// Import the AppKitProvider and required dependencies
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppKitProvider>
+      <App />
+    </AppKitProvider>
   </StrictMode>,
 )
